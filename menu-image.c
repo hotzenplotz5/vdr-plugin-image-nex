eddb8ff182e56f888f50e2e7b58203222b09a5b4
Holger Schvestka <hotzenplotz5@gmx.de>
2026-05-05 23:08:55 +0200

    UI: Add explicit MenuCategory and Id for Skindesigner XML mapping
    Skindesigner failed to map the cMenuImageSkin object to the plug-image_next.xml template because it relied on the localized OSD title, resulting in a fallback to the default list view which rendered the raw tab-separated tokens as cryptic characters. Implemented SetMenuCategory(mcPlugin) and SetId('image_next') to strictly bind the menu to the XML template, regardless of localization.

--- menu-image.c
+++ menu-image.c
@@ -763,6 +763,8 @@ cMenuImageSkinItem::cMenuImageSkinItem(cDirItem *Item) : cOsdItem("") {
 cMenuImageSkin::cMenuImageSkin(cFileSource *Source)
 : cOsdMenu(tr("Image Grid"))
 {
+    SetMenuCategory(mcPlugin);
+    SetId("image_next");
     source = Source;
     list = new cDirList;
     currentdir = NULL;
