6eacec0067f12d5fb34747f9e24b7fc0b674741f
Holger Schvestka <hotzenplotz5@gmx.de>
2026-05-07 09:26:14 +0200

    fix gridview

--- menu-image.c
+++ menu-image.c
@@ -763,7 +763,7 @@ cMenuImageSkinItem::cMenuImageSkinItem(cDirItem *Item) : cOsdItem("") {
 cMenuImageSkin::cMenuImageSkin(cFileSource *Source)
 : cOsdMenu(tr("Images"))
 {
-    SetMenuCategory(mcPlugin);
+    SetMenuCategory(mcUser1);
     /* The following block is commented out to allow compilation on systems
        with inconsistent VDR headers. This will disable the skindesigner XML grid view.
     #if APIVERSNUM >= 20301
