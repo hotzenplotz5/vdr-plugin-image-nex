c564c5e8a09ced7670344f0e7f1b28a47c76ce91
Holger Schvestka <hotzenplotz5@gmx.de>
2026-05-08 19:02:08 +0200

    fix: use standard cOsdItem with tabs (required by skindesigner for VDR >= 2.3.1)

--- menu-image.c
+++ menu-image.c
@@ -48,10 +48,6 @@ extern "C" {
 #include <libswscale/swscale.h>
 }
 
-// Holzhammer-Fix: Absolute Pfade, um kaputte System-Header zu umgehen
-#include "/usr/include/libskindesignerapi/skindesignerapi.h"
-#include "/usr/include/libskindesignerapi/skindesignerosdbase.h"
-
 static cImage* LoadThumbnail(const char* path, int maxWidth, int maxHeight, bool fastOnly = false) {
     uint64_t tStart = cTimeMs::Now();
     esyslog("imageplugin: ---> Start loading thumbnail: %s", path);
@@ -726,7 +722,7 @@ eOSState cMenuImageGrid::ProcessKey(eKeys Key)
 
 // --- cMenuImageSkinItem ---------------------------------------------------
 
-class cMenuImageSkinItem : public cSkindesignerOsdItem {
+class cMenuImageSkinItem : public cOsdItem {
 private:
     cDirItem *item;
 public:
@@ -734,7 +730,7 @@ public:
     cDirItem *Item(void) { return item; }
 };
 
-cMenuImageSkinItem::cMenuImageSkinItem(cDirItem *Item) : cSkindesignerOsdItem("") {
+cMenuImageSkinItem::cMenuImageSkinItem(cDirItem *Item) : cOsdItem("") {
     item = Item;
     char *dirPath = item->Path();
     char *fullDirPath = item->Source->BuildName(dirPath);
@@ -765,10 +761,11 @@ cMenuImageSkinItem::cMenuImageSkinItem(cDirItem *Item) : cSkindesignerOsdItem(""
     
     int is_dir = (item->Type == itDir || item->Type == itParent) ? 1 : 0;
     
-    SetToken("thumbnail", thumbPath ? thumbPath : "");
-    SetToken("albumname", item->DisplayName ? item->DisplayName : "");
-    SetToken("is_folder", is_dir ? "1" : "0");
-    SetText(item->DisplayName ? item->DisplayName : "", true);
+    char *buffer = NULL;
+    if (asprintf(&buffer, "%s\t%s\t%d", thumbPath ? thumbPath : "", item->DisplayName ? item->DisplayName : "", is_dir) >= 0) {
+        SetText(buffer, true);
+        free(buffer);
+    }
 
     if (thumbPath) free(thumbPath);
     free(fullDirPath);
